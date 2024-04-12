50c3ee847a51dd391b4f01c062001f9adc2d89d2
AlekseyGreen <aleksey.green@gmail.com>
2024-04-12 16:53:51 +0300

    в assertions.py добавлены методы assert_json_has_keys, assert_json_has_not_key в Tests добавлен test_user_get.py

--- lib/assertions.py
+++ lib/assertions.py
@@ -23,6 +23,25 @@ class Assertions:
                 f"Response is not JSON format. Response text if '{response.text}'"
 
         assert name in response_as_dict, f"Response JSON doesn't have key {name}"
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, \
+                f"Response is not JSON format. Response text if '{response.text}'"
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn't have key {name}"
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, \
+                f"Response is not JSON format. Response text if '{response.text}'"
+
+        assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}' . But its present"
 
     @staticmethod
     def assert_code_status(response: Response, expected_status_code):
