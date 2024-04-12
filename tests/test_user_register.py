af7692d86e6894a0ed4ad9b938ea4c16247768fe
AlekseyGreen <aleksey.green@gmail.com>
2024-04-12 13:29:25 +0300

    Ex15: Тесты на метод user Список тестов добавленных в test_user_register.py: - Создание пользователя с некорректным email - без символа @ - Создание пользователя без указания одного из полей - с помощью @parametrize необходимо проверить, что отсутствие любого параметра не дает зарегистрировать пользователя - Создание пользователя с очень коротким именем в один символ - Создание пользователя с очень длинным именем - длиннее 250 символов

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,7 +1,8 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
-from  datetime import datetime
+from datetime import datetime
+import pytest
 
 class TestUserRegister(BaseCase):
     def setup_method(self):
@@ -19,14 +20,13 @@ class TestUserRegister(BaseCase):
             "email": self.email
         }
 
-        response = requests.post("https://playground.learnqa.ru/api/user/",
-                                 data=data)
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, 'id')
 
-    def test_create_user_with_existing_email(self):
-        email = 'vinkotov@example.com'
+    @pytest.mark.parametrize('email', ['invalid_email', 'invalid_email@', 'invalid_email.com'])
+    def test_create_user_with_invalid_email(self, email):
         data = {
             'password': '123',
             'username': 'learqa',
@@ -35,9 +35,55 @@ class TestUserRegister(BaseCase):
             "email": email
         }
 
-        response = requests.post("https://playground.learnqa.ru/api/user/",
-                                 data=data)
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
-        assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", \
+        assert response.content.decode('utf-8') == "Invalid email format", f'Unexpected response content: {response.content}'
+
+    @pytest.mark.parametrize('field_to_remove', ['password', 'username', 'firstName', 'lastName', 'email'])
+    def test_create_user_without_field(self, field_to_remove):
+        data = {
+            'password': '123',
+            'username': 'learqa',
+            'firstName': 'learqa',
+            'lastName': 'learqa',
+            "email": self.email
+        }
+        data.pop(field_to_remove)
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode('utf-8') == f"The following required params are missed: {field_to_remove}", \
+            f'Unexpected response content: {response.content}'
+
+    def test_create_user_with_short_username(self):
+        data = {
+            'password': '123',
+            'username': 'a',
+            'firstName': 'learqa',
+            'lastName': 'learqa',
+            "email": self.email
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode('utf-8') == "The value of 'username' field is too short", \
+            f'Unexpected response content: {response.content}'
+
+    def test_create_user_with_long_username(self):
+        long_username = "a" * 251
+        data = {
+            'password': '123',
+            'username': long_username,
+            'firstName': 'learqa',
+            'lastName': 'learqa',
+            "email": self.email
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode('utf-8') == "The value of 'username' field is too long", \
             f'Unexpected response content: {response.content}'
