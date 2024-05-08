892321acc664548a7ab68fc07390ab605da67673
AlekseyGreen <aleksey.green@gmail.com>
2024-05-08 10:21:18 +0300

    Добавлены Allure теги

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -3,7 +3,9 @@ import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
+import allure
 
+@allure.epic('Тесты авторизации')
 class TestUserAuth(BaseCase):
     exclude_params = [
         ('no_cookie'),
@@ -21,6 +23,7 @@ class TestUserAuth(BaseCase):
         self.token = self.get_header(response1, 'x-csrf-token')
         self.user_id_from_auth_method = self.get_json_value(response1, 'user_id')
 
+    @allure.description("Тест успешной авторизации пользователя по email и паролю")
     def test_auth_user(self):
         response2 = MyRequests.get(
             '/user/auth',
@@ -35,6 +38,7 @@ class TestUserAuth(BaseCase):
             "User id from auth method is not equal to user id from check method"
         )
 
+    @allure.description("Тест проверки статуса авторизации без отправки Cookies или токена")
     @pytest.mark.parametrize('condition', exclude_params)
     def test_negative_auth_check(self, condition):
 
